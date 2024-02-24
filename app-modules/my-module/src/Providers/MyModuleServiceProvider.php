95e2a94291b6493601ab4e2a9093cd5b579ade0c
Martin Hettiger <martin@hettiger.com>
2024-02-24 12:21:38 +0100

    Make test pass by adding the workaround

--- app-modules/my-module/src/Providers/MyModuleServiceProvider.php
+++ app-modules/my-module/src/Providers/MyModuleServiceProvider.php
@@ -2,15 +2,34 @@
 
 namespace Modules\MyModule\Providers;
 
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Translation\Translator;
 
 class MyModuleServiceProvider extends ServiceProvider
 {
 	public function register()
 	{
 	}
-	
+
 	public function boot()
 	{
+        $this->callAfterResolving('translator', function ($translator) {
+            if (!($translator instanceof Translator)) {
+                return;
+            }
+
+            foreach (File::directories(__DIR__.'/../../resources/lang/') as $directory) {
+                $locale = basename($directory);
+                $packageValidationLines = collect(Arr::dot(trans("my-module::validation", locale: $locale)))
+                    ->mapWithKeys(fn($value, $key) => ["validation.$key" => $value])
+                    ->toArray();
+
+                if (empty($packageValidationLines)) { continue; }
+
+                $translator->addLines($packageValidationLines, $locale);
+            }
+        });
 	}
 }
